d3e0dc3923f283bf4cd47a79c362489409d9255d
Eric Dai <ericdai123@gmail.com>
2024-03-23 16:02:33 -0500

    Created vector embeddings for MongoDB Database, as well as embedded image URLs into images

--- home.py
+++ home.py
@@ -1,39 +1,34 @@
+from vector_embeddings import create_vector_embedding
 import streamlit as st
-import numpy as np
 from PIL import Image
-import torch
-import clip
 import os
-
-# Function to set up mongoDB
-def setup_mongoDB():
-    # TODO
-    return
-
-# Function to preprocess image for Clip and extract features to create vector embedding
-def create_vector_embedding(image_path):
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load("ViT-B/32", device)
-
-    image = Image.open(image_path)
-    image_input = preprocess(image).unsqueeze(0).to(device)
-    with torch.no_grad():
-        image_features = model.encode_image(image_input)
-    return image_features.cpu().numpy()
+import base64
+from io import BytesIO
 
 # Function to handle main logic of project, user uploading image to search for
 def handle_user_upload():
-    uploaded_file = st.file_uploader("Upload a piece of clothing you want to search for:", type=["jpg", "jpeg", "png"])
+    try:
+        uploaded_file = st.file_uploader("Upload a piece of clothing you want to search for:", type=["jpg", "jpeg", "png"])
 
-    if uploaded_file is not None:
-        # Display the uploaded image
-        st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
+        if uploaded_file is not None:
+            # Display the uploaded image
+            st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
 
-        # Create vector embedding
-        vector_embedding = create_vector_embedding(uploaded_file)
-        print(vector_embedding)
+            # Create vector embedding
+            image = Image.open(uploaded_file)
+            vector_embedding = create_vector_embedding(image)
+            print(vector_embedding)
 
-        # TODO: Store vector_embedding into MongoDB atlas database
+            # TODO: Store vector_embedding into MongoDB atlas database
+    except Exception as e:
+        print(f"Error occurred while uploading image. Error: {e}")
+
+# Function for base64 encoding
+def image_to_base64(image):
+    # Convert the image to base64
+    with BytesIO() as buffer:
+        image.save(buffer, format="PNG")
+        return base64.b64encode(buffer.getvalue()).decode()
 
 # Function to resize images
 def resize_image(image, max_width=300):
@@ -41,7 +36,7 @@ def resize_image(image, max_width=300):
     height_size = int((float(image.size[1]) * float(width_percent)))
     return image.resize((max_width, height_size), Image.Resampling.LANCZOS)
 
-# Code replaced by MongoDB database later on
+# Involve MongoDB
 def display_images(image_paths, urls):
     # Load images and extract features
     images = [Image.open(path) for path in image_paths]
@@ -53,19 +48,19 @@ def display_images(image_paths, urls):
     num_cols = 3
     image_width = 200
     columns = st.columns(num_cols)
-    # num_images_per_row = st.columns(len(images_resized))
 
-    for col, (image, url) in enumerate(zip(images_resized, urls)):
+    for col, (image, url) in enumerate(zip(images, urls)):
         with columns[col % num_cols]:
-            st.image(image, width=image_width, use_column_width=False)
-            shortened_url_name = url.strip().split("&")[0]
-            # Create a clickable link with the shortened name
-            st.markdown(f"[{shortened_url_name}]({url.strip()})", unsafe_allow_html=True)
+            # Encode image as base64
+            image_base64 = image_to_base64(image)
 
+            # Create HTML code for image with clickable link
+            html_code = (f'<a href="{url}" target="_blank"><img src="data:image/png;base64,{image_base64}"'
+                         f' alt="Image" style="width:{image_width}px;height:auto;"></a>')
+
+            # Display the HTML code using st.markdown()
+            st.markdown(html_code, unsafe_allow_html=True)
 def main():
-    # TODO: MongoDB Setup
-    setup_mongoDB()
-
     # Directory containing images - temporary
     images_dir = os.path.join(os.path.dirname(__file__), 'images')
     image_files = os.listdir(images_dir)
